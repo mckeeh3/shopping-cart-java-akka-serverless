3bcf02778c58e8770ddee2043a626949068b2f8a
mckeeh3 <mckeeh3@gmail.com>
2021-11-17 06:37:53 -0500

    update dependency versions

---
+++ src/it/java/cartve/api/ShoppingCartVeIntegrationTest.java
@@ -0,0 +1,78 @@
+package cartve.api;
+
+import cartve.entity.ShoppingCartVeEntity;
+import com.akkaserverless.javasdk.testkit.junit.AkkaServerlessTestKitResource;
+import com.google.protobuf.Empty;
+import io.example.Main;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import static java.util.concurrent.TimeUnit.*;
+
+// This class was initially generated based on the .proto definition by Akka Serverless tooling.
+//
+// As long as this file exists it will not be overwritten: you can maintain it yourself,
+// or delete it so it is regenerated as needed.
+
+// Example of an integration test calling our service via the Akka Serverless proxy
+// Run all test classes ending with "IntegrationTest" using `mvn verify -Pit`
+public class ShoppingCartVeIntegrationTest {
+
+  /**
+   * The test kit starts both the service container and the Akka Serverless proxy.
+   */
+  @ClassRule
+  public static final AkkaServerlessTestKitResource testKit =
+    new AkkaServerlessTestKitResource(Main.createAkkaServerless());
+
+  /**
+   * Use the generated gRPC client to call the service through the Akka Serverless proxy.
+   */
+  private final CartService client;
+
+  public ShoppingCartVeIntegrationTest() {
+    client = testKit.getGrpcClient(CartService.class);
+  }
+
+  @Test
+  public void addItemOnNonExistingEntity() throws Exception {
+    // TODO: set fields in command, and provide assertions to match replies
+    // client.addItem(ShoppingCartVeApi.AddLineItem.newBuilder().build())
+    //         .toCompletableFuture().get(5, SECONDS);
+  }
+
+  @Test
+  public void changeItemOnNonExistingEntity() throws Exception {
+    // TODO: set fields in command, and provide assertions to match replies
+    // client.changeItem(ShoppingCartVeApi.ChangeLineItemQuantity.newBuilder().build())
+    //         .toCompletableFuture().get(5, SECONDS);
+  }
+
+  @Test
+  public void removeItemOnNonExistingEntity() throws Exception {
+    // TODO: set fields in command, and provide assertions to match replies
+    // client.removeItem(ShoppingCartVeApi.RemoveLineItem.newBuilder().build())
+    //         .toCompletableFuture().get(5, SECONDS);
+  }
+
+  @Test
+  public void checkoutCartOnNonExistingEntity() throws Exception {
+    // TODO: set fields in command, and provide assertions to match replies
+    // client.checkoutCart(ShoppingCartVeApi.CheckoutShoppingCart.newBuilder().build())
+    //         .toCompletableFuture().get(5, SECONDS);
+  }
+
+  @Test
+  public void getCartOnNonExistingEntity() throws Exception {
+    // TODO: set fields in command, and provide assertions to match replies
+    // client.getCart(ShoppingCartVeApi.GetShoppingCart.newBuilder().build())
+    //         .toCompletableFuture().get(5, SECONDS);
+  }
+
+  @Test
+  public void removeCartOnNonExistingEntity() throws Exception {
+    // TODO: set fields in command, and provide assertions to match replies
+    // client.removeCart(ShoppingCartVeApi.RemoveShoppingCart.newBuilder().build())
+    //         .toCompletableFuture().get(5, SECONDS);
+  }
+}
